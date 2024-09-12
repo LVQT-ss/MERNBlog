fde7e38c493f1a4543e25f3f98a04e5d756b825e
LVQT <levietquocthinh@gmail.com>
2024-09-12 19:54:00 +0700

    Revert "add Google Oath firebase"
    This reverts commit 7dd8ab99b54391e17489a51eeeaccb69535631ed.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,7 +2,6 @@ import User from "../model/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
 import jwt from 'jsonwebtoken';
-
 export const signup = async (req, res, next) => {
 
 
@@ -19,7 +18,7 @@ export const signup = async (req, res, next) => {
         next(errorHandler(400, 'all fields are requried'))
     }
 
-    const hashPassword = bcryptjs.hashSync(password, 10);
+    const hashPassword = bcrypt.hashSync(password, 10);
 
 
     const newUser = new User({
@@ -79,57 +78,3 @@ export const signin = async (req, res, next) => {
 
 
 }
-
-export const google = async (req, res, next) => {
-    const { email, name, googlePhotoUrl } = req.body;
-    try {
-      const user = await User.findOne({ email });
-      if (user) {
-        const token = jwt.sign(
-          { id: user._id },
-          process.env.JWT_SECRET
-        );
-
-        const { password, ...rest } = user._doc;
-
-        res
-          .status(200)
-          .cookie('access_token', token, {
-            httpOnly: true,
-          })
-          .json(rest);
-
-      } else {
-        const generatedPassword =
-          Math.random().toString(36).slice(-8) +
-          Math.random().toString(36).slice(-8);
-        const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
-        const newUser = new User({
-          username:
-            name.toLowerCase().split(' ').join('') +
-            Math.random().toString(9).slice(-4),
-          email,
-          password: hashedPassword,
-          profilePicture: googlePhotoUrl,
-        });
-
-        await newUser.save();
-
-        const token = jwt.sign(
-          { id: newUser._id},
-          process.env.JWT_SECRET
-        );
-
-        const { password, ...rest } = newUser._doc;
-        
-        res
-          .status(200)
-          .cookie('access_token', token, {
-            httpOnly: true,
-          })
-          .json(rest);
-      }
-    } catch (error) {
-      next(error);
-    }
-  };
