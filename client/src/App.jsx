1ec6b8b437bd3a76aa97fa61632d872fa961d6a4
LVQT <levietquocthinh@gmail.com>
2024-09-13 01:29:54 +0700

    add DashProfile and DashSidebar ocmplete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,31 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import About from './pages/About.jsx';
-import SignIn from './pages/Signin';
-import SignUp from './pages/SignUp.jsx';
-import Projects from './pages/Projects';
-import Dashboard from './pages/Dashboard.jsx';
-import Header from './components/Header.jsx';
-import FooterCom from './components/Footer.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/SignUp.jsx";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard.jsx";
+import Header from "./components/Header.jsx";
+import FooterCom from "./components/Footer.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 function App() {
-
   return (
     <BrowserRouter>
-      <Header/>
-        <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/projects' element={<Projects />} />
-        </Routes>
-        <FooterCom/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <FooterCom />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
