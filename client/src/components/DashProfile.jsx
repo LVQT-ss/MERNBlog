3c0b107be14e5b794d594eaa7e4cf29b0f32d249
LVQT <levietquocthinh@gmail.com>
2024-09-15 15:56:44 +0700

    complete create post UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -237,9 +237,25 @@ export default function DashProfile() {
           defaultValue={currentUser.email} onChange={handleChange}
         />
         <TextInput type="password" id="password" placeholder="password" onChange={handleChange}/>
-        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
-          Update
+        <Button
+          type='submit'
+          gradientDuoTone='purpleToBlue'
+          outline
+          disabled={loading || imageFileUploading}
+        >
+          {loading ? 'Loading...' : 'Update'}
         </Button>
+        {currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+            <Button
+              type='button'
+              gradientDuoTone='purpleToPink'
+              className='w-full'
+            >
+              Create a post
+            </Button>
+          </Link>
+        )}
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span onClick={()=> setShowModal(true)} className="cursor-pointer">Delete Account</span>
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -60,7 +60,7 @@ export const signin = async (req, res, next) => {
         }
 
         // check token
-        const token = jwt.sign({ id: validUser._id,isAdmin:validUser.isAdmin},process.env.JWT_SECRET);
+        const token = jwt.sign({ id: validUser._id, isAdmin:validUser.isAdmin},process.env.JWT_SECRET);
         
         const { password: pass, ...rest } = validUser._doc;
 
