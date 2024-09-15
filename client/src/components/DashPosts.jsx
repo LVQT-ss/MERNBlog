9e1877c034d2136e88f1c63650d29d1387b51b4b
LVQT <levietquocthinh@gmail.com>
2024-09-15 23:27:23 +0700

    show more function for posts

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -8,6 +8,9 @@ export default function DashPosts() {
 
     const { currentUser } = useSelector((state) => state.user);
     const [userPosts, setUserPosts] = useState({})
+    const [showMore, setShowMore] = useState(true);
+    const [showModal, setShowModal] = useState(false);
+    const [postIdToDelete, setPostIdToDelete] = useState('');
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -30,10 +33,29 @@ export default function DashPosts() {
       }, [currentUser._id]);
 
       
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin  && userPosts.length > 0  ? (
+        <>
+        
       <Table hoverable className='shadow-md'>
         <Table.Head>
             <Table.HeadCell>
@@ -103,6 +125,16 @@ export default function DashPosts() {
         ))}
 
       </Table>
+      {showMore && (
+            <button
+              onClick={handleShowMore}
+              className='w-full text-teal-500 self-center text-sm py-7'
+            >
+              Show more
+            </button>
+          )}
+        </>
+      
       ):(
         <p>you nigga has no post yet</p>
       )}
