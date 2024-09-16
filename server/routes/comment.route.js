6d35e94fbafd1d705d868e74e6a270c100ce66c4
LVQT <levietquocthinh@gmail.com>
2024-09-16 14:52:49 +0700

    add edit functiona and to comment component

--- server/routes/comment.route.js
+++ server/routes/comment.route.js
@@ -3,7 +3,7 @@ import { verifyToken } from '../utils/verifyUser.js';
 import {
   createComment,
 //   deleteComment,
-//   editComment,
+  editComment,
   getPostComments,
 //   getcomments,
   likeComment,
@@ -14,4 +14,5 @@ const router = express.Router();
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
+router.put('/editComment/:commentId', verifyToken, editComment);
 export default router;
