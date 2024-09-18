fef1ae7e5ec1a8f551ff1126470612ae4d105714
LVQT <levietquocthinh@gmail.com>
2024-09-18 15:27:08 +0700

    add dashboard overview to the admin  dasboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashSidebar from '../components/DashSidebar';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments';
+import DashboardComp from '../components/DashboardComp';
 
 
 export default function Dashboard(){
@@ -33,7 +34,7 @@ export default function Dashboard(){
     {/* comments  */}
     {tab === 'comments' && <DashComments />}
     {/* dashboard comp */}
-    {/* {tab === 'dash' && <DashboardComp />} */}
+    {tab === 'dash' && <DashboardComp />}
   </div>
   )
 }
