ee31cd7b7c931c90076ae50c652d2fc9bb9481b9
LVQT <levietquocthinh@gmail.com>
2024-09-19 03:00:05 +0700

    attemp ten

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000, // Đặt giới hạn cảnh báo thành 1000KB (1MB)
+  },
   server: {
     proxy: {
       '/api': {
